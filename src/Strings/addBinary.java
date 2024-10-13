948d29814eac49df7a18758ede6958a9544013a1
Abhilash107 <abhilash00003108884@gmail.com>
2024-10-13 13:22:33 +0530

    dsa

--- src/Strings/addBinary.java
+++ src/Strings/addBinary.java
@@ -4,6 +4,28 @@ public class addBinary {
     public static void main(String[] args) {
         long num1 = Long.parseLong("10002");
         //long num1 = Long.parseLong(b);
-        System.out.println(num1);
+        //System.out.println(num1);
+        int[] a = {1, 3, 5, 6};
+        System.out.println(searchInsert(a, 5));
+
+
     }
+
+   
+        public static int searchInsert(int[] nums, int target) {
+            int left = 0; 
+            int right = nums.length;
+        
+            while (left < right) {
+                int mid = left + (right - left) / 2; 
+        
+            if (nums[mid] >= target) {
+                right = mid; 
+            } else {
+                left = mid + 1; 
+            }
+            }
+            return left; 
+        }
+       
 }
