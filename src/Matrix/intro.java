8ff24a7e5b3b856937089a740ceffedf71968e79
Abhilash107 <abhilash00003108884@gmail.com>
2024-11-02 00:10:10 +0530

    matrix

--- src/Matrix/intro.java
+++ src/Matrix/intro.java
@@ -11,7 +11,8 @@ public class intro {
         int col = sc.nextInt();
 
         int mat[][] = new int[row][col];
-        System.out.println(mat.length);
+        //System.out.println(mat.length);
+
         for (int i = 0; i < mat.length; i++) {
             for(int j = 0;j < mat[i].length;j++){
                 mat[i][j] = sc.nextInt();
@@ -24,8 +25,37 @@ public class intro {
             }
         }
         
+        boolean res = isIdentity(mat);
+        System.out.println("\n"+res);
+        //check for sparse
 
+    }
+    public static boolean checkSparse(int mat[][]){
+        int c = 0;
+        int e =0;
+        for(int i = 0;i < mat.length;i++){
+            for (int j = 0; j < mat[i].length; j++) {
+                e++;
+                if(mat[i][j] == 0) c++;
+            }
+        }
 
+        return c > (e/2);
+    }
 
+    public static boolean isIdentity(int mat[][]){
+        // For in case if 0s and 1s
+        // for(int i = 0;i<mat.length;i++){
+        //     if(mat[i][i] != 0) return false;
+        // }
+
+        for(int i = 0;i<mat.length;i++){
+            for(int j = 0; j < mat[i].length; j++) {
+                if(i != j && mat[i][j] != 0) return false;
+                else if(mat[i][j] != 1 && i == j) return false;
+            }
+        }
+
+        return true;
     }
 }
