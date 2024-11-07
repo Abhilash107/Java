ad2db1c3233007c725ff71855627315749c67d1d
Abhilash107 <abhilash00003108884@gmail.com>
2024-11-07 09:59:30 +0530

    dsa

--- src/Arrays/productOfArray.java
+++ src/Arrays/productOfArray.java
@@ -4,23 +4,53 @@ import java.util.Arrays;
 
 public class productOfArray {
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4};
-        int res [] = product(arr);
+        int arr[] = {1,0,3,4};
+        int res [] = newProduct(arr);
         System.out.println(Arrays.toString(res));
     }
 
     public static int[] product(int a[]){
-        int res = 1;
+        
         int newArr[] = new int[a.length];
         for (int i = 0; i < a.length; i++) {
-            int j = 0;
-            while (j !=i && j <a.length) {
-                res *= a[i];j++; 
+            int res = 1;
+            for (int j = 0; j <a.length; j++) {
+                if(j != i) res *= a[j];
             }
-            newArr[j] = res;
+            newArr[i] = res;
             
         }
 
         return newArr;
+    }
+
+
+    public static int[] newProduct(int[] a){
+        int product = 1;
+        int zeroCount = 0;
+        int res[] = new int[a.length];
+
+        for (int i = 0; i < a.length; i++) {
+            if(a[i] == 0) zeroCount++;
+            else product *= a[i]; 
+        }
+
+        if (zeroCount == 2){
+            for (int i = 0; i < a.length; i++) {
+                res[i] = 0;
+            }
+        }
+        else if (zeroCount == 1){
+            for (int i = 0; i < a.length; i++) {
+                if(a[i] == 0) res[i] = product;
+                else res[i] = 0;
+            }  
+        }
+        else{
+            for (int i = 0; i < a.length; i++) {
+                res[i] = product/a[i];
+            }
+        }
+        return res;
     }
 }
