df3221d7f363936b05665e98cb9bd91b2eee0903
Abhilash107 <abhilash00003108884@gmail.com>
2024-11-09 21:09:03 +0530

    hehe

--- src/Arrays/FindDuplicate.java
+++ src/Arrays/FindDuplicate.java
@@ -2,7 +2,26 @@ package Arrays;
 
 public class FindDuplicate {
     public static void main(String[] args) {
+        System.out.println(smallestNumber(1, 2));
+    }
+    public static int smallestNumber(int n, int t) {
+        int x = n;
+        while(product(x)%t != 0){
+            x++;
+        }
+        return x;
         
+        
+    }
+    public static int product(int n){
+        int product = 1;
+        int temp = n;
+        while(temp > 0){
+            if(product == 0) break;
+            product *= (temp%10);
+            temp /= 10;
+        }
+        return product;
     }
     
 }
