acc111f93ba07afb60e8c5c292ee6fe74a0be062
Abhilash107 <abhilash00003108884@gmail.com>
2025-03-18 10:49:26 +0530

    PriorityQueue in JAVA

---
+++ src/Heap/Kclosest.java
@@ -0,0 +1,66 @@
+package Heap;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.PriorityQueue;
+
+class Pair implements Comparable<Pair>{
+    int diff, element;
+    Pair(int d, int n){
+        this.diff = d;
+        this.element = n;
+
+    }
+
+
+    public int compareTo(Pair other) {
+        // Sort by absolute difference in descending order
+        if (this.diff != other.diff) {
+            return other.diff - this.diff; // Max-heap behavior
+        }
+        return other.element - this.element; // If same diff, sort larger number first
+    }
+}
+
+public class Kclosest {
+    public static void main(String[] args) {
+        int a[] = { 5,6,7,8,9};
+        //List<Integer> ans = findClosestElements(a, 3, 7);
+        List<Integer> ans = findClosestElements(a, 3, 7);
+        System.out.println(ans);
+
+
+    }
+
+
+    public static List<Integer> findClosestElements(int[] arr, int k, int x) {
+        PriorityQueue<Pair> q = new PriorityQueue<>();
+
+        for(int num: arr){
+            q.add(new Pair(Math.abs(num - x), num));
+
+            if(q.size() > k) q.poll();
+
+        }
+
+        List<Integer> list = new ArrayList<>();
+        //int size = 0;
+        while(!q.isEmpty()){
+            Pair pair = q.poll();
+            list.add(pair.element);
+        }
+
+        Collections.sort(list);
+        return list;
+
+    }
+
+    //Using Binary Search
+//    public static List<Integer> findClosestElementsII(int[] arr, int k, int x) {
+//
+//
+//
+//
+//
+//    }
