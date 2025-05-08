530b68879eeb2e52dd6ffedee3ffb33501bfa8ca
Abhilash107 <abhilash00003108884@gmail.com>
2025-05-08 20:55:32 +0530

    o/p qns

---
+++ src/OutputQNS/increment.java
@@ -0,0 +1,13 @@
+package OutputQNS;
+
+public class increment {
+    public static void main(String[] args) {
+        int x = 1;
+        //x++;
+        //x = x + 1;
+        //x += 1; // compound assignment or augmented operator
+        x =+ 2;// assignment with unary plus,
+        //it's just assign the value doesn't increment the int
+        System.out.println(x);
+    }
+}
--- src/OutputQNS/oops/q8.java
+++ src/OutputQNS/oops/q8.java
@@ -2,13 +2,45 @@ package OutputQNS.oops;
 
 public class q8 {
     public static void main(String[] args) {
-        try {
-            System.out.println("Hello"+ 1 / 0);
-        }catch (ArithmeticException e){
-            System.out.println("world");
+//1.        try {
+//            System.out.println("Hello"+ 1 / 0);
+//        }catch (ArithmeticException e){
+//            System.out.println("world");
+//        }
+
+//2.        if(true)
+//            break;//Break outside switch or loop (Error)
+
+        short a = 128;
+        short b = (short) ~a;//variable "a" is a short, but ~a becomes an int
+        System.out.println(a + "  "+ b);
+        for(int i = 0; i <= 20;i++){
+            System.out.println(i + "  "+ ~i);
+
         }
 
-//        if(true)
-//            break;//Break outside switch or loop (Error)
+
+
     }
 }
+//        0  -1
+//        1  -2
+//        2  -3
+//        3  -4
+//        4  -5
+//        5  -6
+//        6  -7
+//        7  -8
+//        8  -9
+//        9  -10
+//        10  -11
+//        11  -12
+//        12  -13
+//        13  -14
+//        14  -15
+//        15  -16
+//        16  -17
+//        17  -18
+//        18  -19
+//        19  -20
+//        20  -21
