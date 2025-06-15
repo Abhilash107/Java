01391e77f03bf12da4bdc62f41937bff7cc5c917
Abhilash107 <abhilash00003108884@gmail.com>
2025-06-15 14:31:54 +0530

    Multithreading in JAVA

--- src/Generics/Test.java
+++ src/Generics/Test.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 public class Test {
     public static void main(String[] args) {
         ArrayList list = new ArrayList<>();
+        //Not type safe as any data type can be added to this
+
 
         list.add("Hello");
         list.add(1);
@@ -21,9 +23,21 @@ public class Test {
         //2.
         Integer integer = (Integer)list.get(1);
         String string = (String)list.get(1);// error
-        //Exception in thread "main" java.lang.ClassCastException: class java.lang.Integer cannot be cast to class java.lang.String (java.lang.Integer and java.lang.String are in module java.base of loader 'bootstrap' at Generics.Test.main(Test.java:21)
+        //Exception in thread "main" java.lang.ClassCastException:
+        //class java.lang.Integer cannot be cast to class java.lang.String
+        // (java.lang.Integer and java.lang.String are in module
+        // java.base of loader 'bootstrap' at Generics.Test.main(Test.java:21)
 
         System.out.println(integer+"  "+string);
+
+        // To overcome this issue we use generics
+        ArrayList<Integer> intList = new ArrayList<>();// <> diamond operator
+
+        intList.add(1);
+        intList.add(2);
+        intList.add(2);
+        intList.add(3);
+        //intList.add("Hello");//error
 
 
 
