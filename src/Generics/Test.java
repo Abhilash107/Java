a6e995686891a0a010cc9214f56e0a1d202279e6
Abhilash107 <abhilash00003108884@gmail.com>
2025-06-15 14:26:57 +0530

    Multithreading in JAVA

--- src/Generics/Test.java
+++ src/Generics/Test.java
@@ -12,16 +12,19 @@ public class Test {
         list.add(2);
         list.add(3);
 
+        //1.
         System.out.println(list);
         Object str = list.get(0);
         String s = (String) list.get(0);
         System.out.println(str+"  "+s);
 
+        //2.
         Integer integer = (Integer)list.get(1);
         String string = (String)list.get(1);// error
         //Exception in thread "main" java.lang.ClassCastException: class java.lang.Integer cannot be cast to class java.lang.String (java.lang.Integer and java.lang.String are in module java.base of loader 'bootstrap' at Generics.Test.main(Test.java:21)
 
         System.out.println(integer+"  "+string);
+
 
 
 
