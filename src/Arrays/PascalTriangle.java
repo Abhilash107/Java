9e719e30f7e89819ac6c0362815c66227beddb52
Abhilash107 <abhilash00003108884@gmail.com>
2025-07-01 22:10:30 +0530

    DSA

---
+++ src/Arrays/PascalTriangle.java
@@ -0,0 +1,30 @@
+package Arrays;
+import java.util.List;
+import java.util.ArrayList;
+public class PascalTriangle {
+
+    public static void main(String[] args) {
+
+
+    }
+    public List<List<Integer>> generate(int n) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int row = 1; row <= n; row++) {
+            ans.add(generateRow(row));
+        }
+        return ans;
+    }
+    public static List<Integer> generateRow(int row) {
+        long ans = 1;
+        List<Integer> ansRow = new ArrayList<>();
+        ansRow.add(1);
+
+        for (int col = 1; col < row; col++) {
+            ans = ans * (row - col);
+            ans = ans / col;
+            ansRow.add((int)ans);
+        }
+        return ansRow;
+    }
+}
