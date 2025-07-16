e110a1172f67a131194562393ed23dad89393212
Abhilash107 <abhilash00003108884@gmail.com>
2025-07-16 22:48:39 +0530

    add MinimumElementInRotatedArray class with binary search implementation

---
+++ src/BinarySearch/MinimumElementInRotatedArray.java
@@ -0,0 +1,31 @@
+package BinarySearch;
+
+public class MinimumElementInRotatedArray {
+    public static void main(String[] args) {
+
+    }
+    public static int findMin(int[] a) {
+        int n = a.length;
+
+        int l = 0, h = n - 1;
+        int ans = Integer.MAX_VALUE;
+
+        while(l <= h){
+            int mid = l + (h - l)/2;
+
+            if(a[l] <= a[mid]){
+                ans = Math.min(ans, a[l]);
+
+                l = mid + 1;
+            }
+            else{
+                ans = Math.min(ans, a[mid]);
+                h = mid - 1;
+            }
+
+
+        }
+
+        return ans;
+    }
+}
