d5d4c71bf4e85568e5077c12cdee8789bcfeab49
Abhilash107 <abhilash00003108884@gmail.com>
2025-08-19 15:28:29 +0530

    add Test1 class with constructor and instantiate in Main class

--- src/OutputQNS/Test.java
+++ src/OutputQNS/Test.java
@@ -14,7 +14,25 @@ class Test {
     protected int x, y;
 }
 
+class Test1 {
+
+    // Constructor of this class
+    Test1(int x)
+    {
+
+        // Print statement whenever this constructor is
+        // called
+        System.out.println("Constructor called " + x);
+    }
+}
+
+
 class Main {
+
+    Test1 t1 = new Test1(10);
+    Main(int i) { t1 = new Test1(i); }
+
+
     public static void main(String args[]) {
 //        Test t = new Test();
 //        System.out.println(t.x + " " + t.y);// 0 0
@@ -37,6 +55,13 @@ class Main {
         //sol:
         //1. Remove the parameterized constructor.
         //2. Add a constructor without any parameter.
+
+
+
+        //Main obj = new Main(5);
+        //Constructor called 10
+        //Constructor called 5
+
 
 
 
