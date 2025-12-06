dfceb19d3ca55d61bcbbed5c9b444a22d1f4763c
Abhilash107 <abhilash00003108884@gmail.com>
2025-12-06 11:28:40 +0530

    DSA

--- src/Strings/mostAskedQns/RemoveDuplicates.java
+++ src/Strings/mostAskedQns/RemoveDuplicates.java
@@ -1,16 +1,17 @@
 package Strings.mostAskedQns;
 
-import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
         String s = "banana";
-        System.out.println(result(s));
+        System.out.println(resultI(s));
+        System.out.println(resultII(s));
+        System.out.println(resultIII(s));
     }
 
-    public static String result(String ip){
-        String res = "";
+    public static String resultI(String ip){
+        StringBuilder res = new StringBuilder();
         LinkedHashSet<Character> set = new LinkedHashSet<>();
 
         for(char c: ip.toCharArray()){
@@ -18,25 +19,44 @@ public class RemoveDuplicates {
         }
 
         for(char c: set){
-            res += c;
+            res.append(c);;
         }
 
-        return res;
+        return res.toString();
 
     }
 
     public static String resultII(String ip){
-        String res = "";
-        LinkedHashSet<Character> set = new LinkedHashSet<>();
+        StringBuilder res = new StringBuilder();
+        int freq[] = new int[256];
+
 
         for(char c: ip.toCharArray()){
-            set.add(c);
+
+            if(freq[c] == 0){
+                res.append(c);
+                freq[c] = 1;
+            }
         }
 
-        for(char c: set){
-            res += c;
+
+        return res.toString();
+    }
+
+    public static String resultIII(String ip){
+        StringBuilder res = new StringBuilder();
+        boolean visited[] = new boolean[256];
+
+
+        for(char c: ip.toCharArray()){
+
+            if(!visited[c]){
+                res.append(c);
+                visited[c] = true;
+            }
         }
 
-        return res;
+
+        return res.toString();
     }
 }
