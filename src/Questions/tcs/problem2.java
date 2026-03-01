d6829d538aaeeef59393b9702f666d53460290e1
Abhilash Mishra <abhilash00003108884@gmail.com>
2026-03-01 21:06:29 +0530

    Changes

--- src/Questions/tcs/problem2.java
+++ src/Questions/tcs/problem2.java
@@ -1,5 +1,25 @@
 package Questions.tcs;
 
-public class problem2 {
+import java.util.*;
 
+public class problem2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s= sc.next();
+        int val = sc.nextInt();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0;i < s.length();i++){
+            char ch = s.charAt(i);
+            if(Character.isLowerCase(ch))sb.append((ch - 'a' + val)%26 + 'a');
+
+            else if(Character.isUpperCase(ch))sb.append((ch - 'A' + val)%26 + 'A');
+
+            else if(Character.isDigit(ch))sb.append((ch - '0' + val)%10 + '0');
+            else sb.append(ch == '@' ? '#' : '@');
+        }
+
+        System.out.println(sb.toString());
+    }
 }
